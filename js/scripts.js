93bcd7decf0ff5314556d5e73e98b539e2052e8e
ab5562 <ab5562@nyu.edu>
2017-04-06 14:13:56 -0400

    finishing styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-//instantiate map into container with set view
+//instantiate map into container with set view, turn off scroll wheel
 var map = L.map('mapcontainer', {
 	scrollWheelZoom: false,
-}).setView([40.007960, -75.125049], 11);
+}).setView([40.007960, -75.055049], 11);
 //add carto light basemap
 //var layer =  
 L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
@@ -19,7 +19,6 @@ function getColor(d) {
 			d > 14 ? 	'#FECC5C' :
 						'#FFFFB2';
 }
-
 function style(feature){
 	return{
 		fillColor: getColor(feature.properties.PercPov),
@@ -29,7 +28,6 @@ function style(feature){
 		fillOpacity: 0.7
 	};
 }
-
 //add poverty choropleth to map
 var poverty = L.geoJson(neighborhoods, {style: style}).addTo(map);
 
@@ -54,26 +52,14 @@ function style2(feature){
 	};
 }
 
-//add poverty choropleth to map
+//add nonwhite choropleth to map
 var nonwhite = L.geoJson(neighborhoods, {style: style2}).addTo(map);
-
+//add walking buffer to map
 var bufferOutline = L.geoJson(buffer).addTo(map);
-
+//add station locations to map
 var stationMarkers = L.geoJson(stations, {
     pointToLayer: function (feature, latlng) {
-
-
-    //create dynamic circle marker radius based on the number of docks at a station
-      function getRadius(feature) {
-        if (feature.properties.totalDocks > 20) return 10;
-        if (feature.properties.totalDocks > 15) return 7.5;
-        if (feature.properties.totalDocks > 10) return 5;
-        if (feature.properties.totalDocks > 5) return 2.5;
-        if (feature.properties.totalDocks > 0) return 1;
-      }
-
       var geojsonMarkerOptions = {
-        //radius: getRadius(feature),
         radius: 3,
         fillColor: "deepskyblue",
         color: "gray",
@@ -82,22 +68,15 @@ var stationMarkers = L.geoJson(stations, {
         fillOpacity: 0.8,
         z: 1
       };
-
       return L.circleMarker(latlng, geojsonMarkerOptions);
     }
-//}).bindPopup(function (layer) {
-//    return (' Total number of docks in kiosk: ' + layer.feature.properties.totalDocks +
-//    		' Docks currently available: ' + layer.feature.properties.docksAvailable + 
-//     		' Bikes currently available: ' + layer.feature.properties.bikesAvailable);  
 }).addTo(map);
 
 //define base and overlay layers for control
-
 var baseLayers = {
 	"Neighborhoods - percent in poverty": poverty,
 	"Neighborhoods - percent not white": nonwhite
 };
-
 var overlays = {
 	"Half Mile Walking Buffer": bufferOutline,
 	"Indego Kiosks": stationMarkers
@@ -105,13 +84,17 @@ var overlays = {
 //add control to map
 L.control.layers(baseLayers,overlays,{
 	collapsed:false,
-	position:'bottomright'
+	position:'topright'
 }).addTo(map);
 
 
-// Set up an invisible neighborhoods layer.  
-//   This will allow users to click on neighborhoods for info
+// Set up an invisible neighborhoods layer and bind popup
 var neighborhoodinfo = L.geoJson(neighborhoods, {
 	opacity: 0,
 	fillOpacity: 0
+}).bindPopup(function (layer) {
+    return (' Neighborhood Name: ' + layer.feature.properties.FinalName +
+    		'</br> Percent in poverty: ' + layer.feature.properties.PercPov + '%' +
+    		'</br> Percent not white: ' + layer.feature.properties.PercNW + '%' +
+    		'</br> Number of Indego kiosks: ' + layer.feature.properties.Join_Count); // +
 }).addTo(map);
